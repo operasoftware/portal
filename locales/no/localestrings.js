5d9b1b29c85d041b3347383c0d24681664668ede
Per Øyvind Øygard <peroyo@gmail.com>
2012-05-02 16:30:43 +0200

    Added Norwegian translation

---
+++ locales/no/localestrings.js
@@ -0,0 +1,10 @@
+_STRINGS = {
+    "Loading": "Laster",
+    "Seconds Per Post": "Sekunder per innlegg",
+    "seconds": "sekunder",
+    "Sources": "Kilder",
+    "posts": "innlegg",
+    "Save": "Lagre",
+    "Note": "Merk",
+    "To change language or add more sources, please go to": "For å endre språk eller legge til flere kilder, gå til"
+}
